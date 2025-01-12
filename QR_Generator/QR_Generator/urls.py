797fa77281f56aede9a1c2bb6881a10e89c496d9
Gleb604 <gleb29032006@gmail.com>
2025-01-12 11:28:58 +0100

    Merge

--- QR_Generator/QR_Generator/urls.py
+++ QR_Generator/QR_Generator/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from qrcode_app.views import *
-from contacts_app.views import *
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("home_app.urls")),
     path("qrcode/", include("qrcode_app.urls")),
-    path("contacts/", include("contacts_app.urls"))
+    path("contacts/", include("contacts_app.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- QR_Generator/qrcode_app/views.py
+++ QR_Generator/qrcode_app/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from .models import QRCode
+from qrcode import make
+import os
+# Create your views here.
 
 # Create your views here.
 
 def render_generator_qr(request):
     return render(request = request, template_name = "generator_qr/generator_qr.html")
+    if request.method == 'POST':
+        data = request.POST['data']
+        name = request.POST['name']
+        img = make(data, version = request.POST['size'], box_size = 40, )
+        img.save(os.path.abspath(__file__ + f"/../../media/qrcodes/{name}.png"))
+        model = QRCode(name = name , size = request.POST['size'] , color = request.POST['color'], form = request.POST['form'], link = request.POST['data'], path_qrcode = f"qrcodes/{name}.png")
+        model.save()
+        return render(request = request, template_name = "generator_qr/generator_qr.html", context = {"Qrcode": model})
+    else:
+        pass
+
+    return render(request = request, template_name = "generator_qr/generator_qr.html")
+
+def view_my_qrcodes(request):
+    all_qrcodes = QRCode.objects.all()
+    return render(request, "my_qrcodes.html", context = {"all_qrcodes": all_qrcodes} )
